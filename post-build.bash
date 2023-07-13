a01cab9c7b6815d3361bf0bce338d52e7424964a
AnnatarHe <annatar.he+github@gmail.com>
2023-07-14 01:37:23 +0800

    fix(ci): fix binary sign

--- post-build.bash
+++ post-build.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-AMD_DIST=./dist/pp-mac_darwin_amd64_v1/promptpal
-ARM_DIST=./dist/pp-mac_darwin_arm64/promptpal
+AMD_DIST="./dist/pp-mac_darwin_amd64_v1/promptpal"
+ARM_DIST="./dist/pp-mac_darwin_arm64/promptpal"
 
-if [ -f "$ARM_DIST" && -f "$AMD_DIST" ]; then
-	gon .gon.hcl
-else 
-	echo "waiting for build to finish..."
+echo "Executing gon..."
+
+if [ -f "$ARM_DIST" ] && [ -f "$AMD_DIST" ]; then
+    gon .gon.hcl
+else
+    echo "Waiting for build to finish..."
 fi
